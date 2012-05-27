1d3a565bef31d05ee90a461d1310403c65f62c52
mgutz <mario@mgutz.com>
2012-05-27 14:02:06 -0700

    added funcd

--- test/consolidate.js
+++ test/consolidate.js
@@ -1,15 +1,16 @@
 
-require('./shared').test('jade');
+require('./shared').test('dust');
+require('./shared').test('eco');
 require('./shared').test('ejs');
-require('./shared').test('swig');
+require('./shared').test('funcd');
+require('./shared').test('haml');
+require('./shared').test('haml-coffee');
+require('./shared').test('handlebars');
+require('./shared').test('hogan');
+require('./shared').test('jade');
 require('./shared').test('jazz');
 require('./shared').test('jqtpl');
 require('./shared').test('liquor');
-require('./shared').test('haml');
-require('./shared').test('eco');
-require('./shared').test('whiskers');
-require('./shared').test('haml-coffee');
-require('./shared').test('hogan');
-require('./shared').test('dust');
-require('./shared').test('handlebars');
+require('./shared').test('swig');
 require('./shared').test('underscore');
+require('./shared').test('whiskers');
